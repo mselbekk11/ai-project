9bc44442fb4ba41a932ff77feb90fd5ce708cdb2
mselbekk11 <mselbekk11@gmail.com>
2025-03-17 16:21:09 -0700

    updates

--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -23,7 +23,7 @@ export default function Page({ children }: Props) {
     <SidebarProvider>
       <AppSidebar />
       <SidebarInset>
-        <header className="flex h-16 shrink-0 items-center gap-2 border-b">
+        <header className="sticky top-0 z-10 flex h-16 shrink-0 items-center gap-2 border-b bg-background">
           <div className="flex items-center justify-between w-full px-3">
             <div className="flex items-center gap-2">
               <SidebarTrigger />
