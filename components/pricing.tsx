fc2558a59f1538adabea55be87cf53ae40cd66e1
mselbekk11 <mselbekk11@gmail.com>
2025-04-04 20:18:44 -0700

    updates

--- components/pricing.tsx
+++ components/pricing.tsx
@@ -24,12 +24,13 @@ export default function Pricing({
       <div className="mx-auto max-w-6xl px-6 w-full">
         <div className="mx-auto max-w-6xl space-y-6 text-center">
           <h1 className="mt-8 text-4xl font-extrabold md:text-5xl xl:text-4xl xl:[line-height:1.125]">
-            Select a Package
+            Buy some Credits
           </h1>
           <p className="mx-auto mt-8 hidden max-w-2xl text-wrap text-md sm:block">
-            Pay once, no subscriptions or hidden fees. We offer no trial due to
-            high costs, <br /> but we will refund you if you&apos;re
-            unsatisfied.
+            Pay once,{" "}
+            <span className="font-bold">no subscriptions or hidden fees</span>.
+            We do not offer trials due to high costs, <br /> but we will refund you
+            if you&apos;re unsatisfied.
           </p>
         </div>
 
@@ -40,7 +41,7 @@ export default function Pricing({
               <span className="my-3 block text-2xl font-semibold">$19</span>
             </CardHeader>
 
-            <CardContent className="space-y-4">
+            <CardContent className="flex-grow space-y-4">
               <hr className="border-dashed" />
 
               <ul className="list-outside space-y-3 text-sm">
@@ -57,7 +58,7 @@ export default function Pricing({
               </ul>
             </CardContent>
 
-            <CardFooter className="mt-auto">
+            <CardFooter>
               <Button
                 variant="outline"
                 className="w-full"
@@ -76,69 +77,23 @@ export default function Pricing({
             </CardFooter>
           </Card>
 
-          <Card className="relative">
+          <Card className="relative flex flex-col">
             <span className="bg-purple-600 absolute inset-x-0 -top-3 mx-auto flex h-6 w-fit items-center rounded-full px-3 py-1 text-xs font-medium text-white ring-1 ring-inset ring-white/40 ring-offset-1 ring-offset-gray-950/5">
               Popular
             </span>
 
-            <div className="flex flex-col">
-              <CardHeader>
-                <CardTitle className="font-medium">Pro</CardTitle>
-                <span className="my-3 block text-2xl font-semibold">$29</span>
-              </CardHeader>
-
-              <CardContent className="space-y-4">
-                <hr className="border-dashed" />
-                <ul className="list-outside space-y-3 text-sm">
-                  {[
-                    "2 Model Credits",
-                    "40 Clothing Credits",
-                    "40 Generation Credits",
-                    "Priority Support",
-                  ].map((item, index) => (
-                    <li key={index} className="flex items-center gap-2">
-                      <Check className="size-3" />
-                      {item}
-                    </li>
-                  ))}
-                </ul>
-              </CardContent>
-
-              <CardFooter>
-                <Button
-                  className="w-full"
-                  onClick={() => handlePurchase("pro")}
-                  disabled={isProcessing}
-                >
-                  {isProcessing && processingPlan === "pro" ? (
-                    <>
-                      <Loader2 className="mr-2 h-4 w-4 animate-spin" />
-                      Processing...
-                    </>
-                  ) : (
-                    "Get Started"
-                  )}
-                </Button>
-              </CardFooter>
-            </div>
-          </Card>
-
-          <Card className="flex flex-col">
             <CardHeader>
-              <CardTitle className="font-medium">Premium</CardTitle>
-              <span className="my-3 block text-2xl font-semibold">$49</span>
+              <CardTitle className="font-medium">Pro</CardTitle>
+              <span className="my-3 block text-2xl font-semibold">$29</span>
             </CardHeader>
 
-            <CardContent className="space-y-4">
+            <CardContent className="flex-grow space-y-4">
               <hr className="border-dashed" />
-
               <ul className="list-outside space-y-3 text-sm">
                 {[
-                  "3 Model Credits",
-                  "60 Clothing Credits",
-                  "60 Generation Credits",
-                  "Priority Support",
-                  "Early Access to New Features",
+                  "2 Model Credits",
+                  "40 Clothing Credits",
+                  "40 Generation Credits",
                 ].map((item, index) => (
                   <li key={index} className="flex items-center gap-2">
                     <Check className="size-3" />
@@ -148,7 +103,48 @@ export default function Pricing({
               </ul>
             </CardContent>
 
-            <CardFooter className="mt-auto">
+            <CardFooter>
+              <Button
+                className="w-full"
+                onClick={() => handlePurchase("pro")}
+                disabled={isProcessing}
+              >
+                {isProcessing && processingPlan === "pro" ? (
+                  <>
+                    <Loader2 className="mr-2 h-4 w-4 animate-spin" />
+                    Processing...
+                  </>
+                ) : (
+                  "Get Started"
+                )}
+              </Button>
+            </CardFooter>
+          </Card>
+
+          <Card className="flex flex-col">
+            <CardHeader>
+              <CardTitle className="font-medium">Premium</CardTitle>
+              <span className="my-3 block text-2xl font-semibold">$49</span>
+            </CardHeader>
+
+            <CardContent className="flex-grow space-y-4">
+              <hr className="border-dashed" />
+
+              <ul className="list-outside space-y-3 text-sm">
+                {[
+                  "3 Model Credits",
+                  "60 Clothing Credits",
+                  "60 Generation Credits",
+                ].map((item, index) => (
+                  <li key={index} className="flex items-center gap-2">
+                    <Check className="size-3" />
+                    {item}
+                  </li>
+                ))}
+              </ul>
+            </CardContent>
+
+            <CardFooter>
               <Button
                 variant="outline"
                 className="w-full"
