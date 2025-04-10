cf0b2fe1b5b7e09402efca8961f287e3e1aea79a
mselbekk11 <mselbekk11@gmail.com>
2025-04-09 20:41:50 -0700

    updates

--- components/navigation.tsx
+++ components/navigation.tsx
@@ -18,6 +18,7 @@ const menuItems = [
   { name: "Solution", href: "#" },
   { name: "Pricing", href: "#" },
   { name: "About", href: "#" },
+  { name: "About", href: "#" },
 ];
 
 export default function Navigation({
@@ -28,14 +29,14 @@ export default function Navigation({
   setMenuState: (menuState: boolean) => void;
 }) {
   return (
-    <header className="sticky top-0 z-40 bg-white">
+    <header className="w-full absolute top-0 z-40 bg-transparent">
       <nav
         data-state={menuState && "active"}
-        className="z-20 w-full backdrop-blur md:relative dark:bg-zinc-950/50 lg:dark:bg-transparent shadow-md"
+        className="z-20 w-full md:relative dark:bg-transparent lg:dark:bg-transparent"
       >
         <div className="m-auto max-w-5xl px-6">
           <div className="flex flex-wrap items-center justify-between gap-6 py-3 lg:gap-0 lg:py-4">
-            <div className="flex w-full justify-between lg:w-auto">
+            <div className="flex justify-start lg:w-[180px]">
               <Link
                 href="/"
                 aria-label="home"
@@ -49,20 +50,64 @@ export default function Navigation({
                   Trizzy
                 </span>
               </Link>
+            </div>
 
+            <div className="flex lg:hidden">
               <button
                 onClick={() => setMenuState(!menuState)}
                 aria-label={menuState == true ? "Close Menu" : "Open Menu"}
-                className="relative z-20 -m-2.5 -mr-4 block cursor-pointer p-2.5 lg:hidden"
+                className="relative z-20 -m-2.5 block cursor-pointer p-2.5"
               >
                 <Menu className="in-data-[state=active]:rotate-180 in-data-[state=active]:scale-0 in-data-[state=active]:opacity-0 m-auto size-6 duration-200" />
                 <X className="in-data-[state=active]:rotate-0 in-data-[state=active]:scale-100 in-data-[state=active]:opacity-100 absolute inset-0 m-auto size-6 -rotate-180 scale-0 opacity-0 duration-200" />
               </button>
             </div>
 
-            <div className="bg-background in-data-[state=active]:block lg:in-data-[state=active]:flex mb-6 hidden w-full flex-wrap items-center justify-end space-y-8 rounded-3xl border p-6 shadow-2xl shadow-zinc-300/20 md:flex-nowrap lg:m-0 lg:flex lg:w-fit lg:gap-6 lg:space-y-0 lg:border-transparent lg:bg-transparent lg:p-0 lg:shadow-none dark:shadow-none dark:lg:bg-transparent">
-              <div className="lg:pr-4">
-                <ul className="space-y-6 text-base lg:flex lg:gap-8 lg:space-y-0 lg:text-sm">
+            <div className="hidden lg:flex lg:flex-1 lg:justify-center">
+              <ul className="flex items-center gap-8 text-sm">
+                {menuItems.map((item, index) => (
+                  <li key={index}>
+                    <Link
+                      href={item.href}
+                      className="text-muted-foreground hover:text-accent-foreground block duration-150"
+                    >
+                      <span>{item.name}</span>
+                    </Link>
+                  </li>
+                ))}
+              </ul>
+            </div>
+
+            <div className="hidden lg:flex lg:w-[180px] lg:items-center lg:justify-end lg:gap-3">
+              <SignedOut>
+                <SignInButton>
+                  <Button size="sm">Sign in</Button>
+                </SignInButton>
+
+                <SignUpButton>
+                  <Button size="sm" variant="outline" className="">
+                    Sign Up
+                  </Button>
+                </SignUpButton>
+              </SignedOut>
+              <SignedIn>
+                <SignOutButton>
+                  <Button variant="outline" size="sm">
+                    Sign out
+                  </Button>
+                </SignOutButton>
+
+                <Link href="/home">
+                  <Button variant="default" size="sm">
+                    Dashboard
+                  </Button>
+                </Link>
+              </SignedIn>
+            </div>
+
+            <div className="bg-background in-data-[state=active]:block mb-6 hidden w-full flex-wrap items-center justify-end space-y-8 rounded-3xl border p-6 shadow-2xl shadow-zinc-300/20 md:flex-nowrap lg:hidden">
+              <div>
+                <ul className="space-y-6 text-center text-base">
                   {menuItems.map((item, index) => (
                     <li key={index}>
                       <Link
@@ -76,29 +121,29 @@ export default function Navigation({
                 </ul>
               </div>
 
-              <div className="flex w-full flex-col space-y-3 sm:flex-row sm:gap-3 sm:space-y-0 md:w-fit lg:border-l lg:pl-6">
+              <div className="flex w-full flex-col space-y-3 sm:flex-row sm:gap-3 sm:space-y-0 md:w-fit sm:justify-center">
                 <SignedOut>
                   <SignInButton>
-                    <Button size="sm">Sign in</Button>
+                    <Button size="sm" className="w-full">
+                      Sign in
+                    </Button>
                   </SignInButton>
 
                   <SignUpButton>
-                    <Button size="sm" variant="outline" className="">
+                    <Button size="sm" variant="outline" className="w-full">
                       Sign Up
                     </Button>
                   </SignUpButton>
                 </SignedOut>
                 <SignedIn>
-                  {/* <UserButton />
-                   */}
                   <SignOutButton>
-                    <Button variant="outline" size="sm">
+                    <Button variant="outline" size="sm" className="w-full">
                       Sign out
                     </Button>
                   </SignOutButton>
 
-                  <Link href="/home">
-                    <Button variant="default" size="sm">
+                  <Link href="/home" className="w-full">
+                    <Button variant="default" size="sm" className="w-full">
                       Dashboard
                     </Button>
                   </Link>
