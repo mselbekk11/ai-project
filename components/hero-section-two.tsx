4b44cb28ac1f551b36268616e4a5cb73d4613f8c
mselbekk11 <mselbekk11@gmail.com>
2025-04-25 13:21:37 -0700

    updates

--- components/hero-section-two.tsx
+++ components/hero-section-two.tsx
@@ -207,7 +207,7 @@ export default function HeroSectionTwo() {
         />
       </div> */}
 
-      <div className="relative py-24 lg:pt-44 lg:pb-52">
+      <div className="relative py-24 lg:pt-44 lg:pb-44">
         <div className="mx-auto max-w-7xl px-6 md:px-12">
           <div className="text-center sm:mx-auto sm:w-10/12 lg:mr-auto lg:mt-0 lg:w-4/5">
             {/* Pill Component */}
